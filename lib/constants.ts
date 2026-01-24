69d58bce5e6f8f9c60abbdf9dc28fcd129fbfcf7
Lailaolab Dev <136585782+lailaolabdev@users.noreply.github.com>
2026-01-24 23:09:30 +0700

    chore: Update default API_BASE_URL to `https://api.fiwfans.win`.

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://api.fiwfans.win";
 // export const API_BASE_URL = "http://localhost:8000";
